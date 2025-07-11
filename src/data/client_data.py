85bce9bba70dc18c74903c047ec94f06f03762dd
Ashis Khan <ashis@eyefit.ai>
2025-07-11 17:10:27 +0530

    requirements.txt updated and DB_PORT added in get_db generator

--- src/data/client_data.py
+++ src/data/client_data.py
@@ -11,6 +11,7 @@ def get_db():
         connection = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
+            port=os.getenv("DB_PORT"),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME")
         )
